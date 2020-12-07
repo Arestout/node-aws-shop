e52e0dff9cb766cee28df366b42148b5adef3f79
Anton Kablukov <antoha7@gmail.com>
2020-12-07 11:16:13 +0100

    refactor: change api path to gateway with proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://8lcxyejzqd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://pjberqbx41.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://arestout-cart-api-develop.eu-west-1.elasticbeanstalk.com/api',
+  cart: 'https://nx3pi5e3pj.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
 export default API_PATHS;
